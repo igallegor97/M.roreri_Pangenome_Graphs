32b2da50ae1426eb69faf2a3fb1e27e54e377450
Isabella Gallego Rendón <88630062+igallegor97@users.noreply.github.com>
2025-04-09 21:57:31 -0300

    Update and rename annot_quality.py to annot_quality.sh
    Removed the previous code, added the new full pipeline

---
+++ annot_quality.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# CONFIG 
+GENOME="Mror_C26.illumina.fasta"
+GFF="Mror_C26.illumina.gff3"
+PREFIX="C26"
+WORKDIR="/home/isagallegor/M_roreri_pan"
+
+# ENTER WORKDIR
+cd "$WORKDIR" || exit
+
+# 1. EXTRACT CDS WITH GFFREAD
+echo "Extracting CDS with gffread"
+gffread "$GFF" -g "$GENOME" -x output_CDS_"$PREFIX".fasta
+
+# 2. TRANSLATE CDS TO PROTEINS WITH TRANSEQ
+echo "Translating CDS to proteins with transeq"
+transeq -sequence output_CDS_"$PREFIX".fasta -outseq output_proteins_"$PREFIX".fasta
+
+# 3. Filter proteins (one per gene) with Biopython
+echo "Filtering"
+python3 <<EOF
+from Bio import SeqIO
+from collections import defaultdict
+
+input_file = "output_proteins_${PREFIX}.fasta"
+output_file = "final_proteome_${PREFIX}.fasta"
+
+gene_dict = defaultdict(list)
+for record in SeqIO.parse(input_file, "fasta"):
+    gene_id = record.id.split(".")[0]
+    gene_dict[gene_id].append(record)
+
+selected_records = []
+for records in gene_dict.values():
+    longest = max(records, key=lambda x: len(x.seq))
+    selected_records.append(longest)
+
+SeqIO.write(selected_records, output_file, "fasta")
+print("Final proteome written in", output_file)
+EOF
+
+echo "Done"
