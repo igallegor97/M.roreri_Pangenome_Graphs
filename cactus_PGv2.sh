b6ae4f5d1b538a95c7fbb116effb4ecf1e52c494
Isabella Gallego Rendón <88630062+igallegor97@users.noreply.github.com>
2025-06-27 16:47:15 -0300

    Update cactus_PGv2.sh

--- cactus_PGv2.sh
+++ cactus_PGv2.sh
@@ -7,4 +7,4 @@
 
 module load singularity-ce/3.11.2
 
-taskset -c 0-${NSLOTS} singularity exec -H $(pwd) cactus_v2.9.9.sif cactus-pangenome ./tt_v2 data/Mror_Genomes.txt --outDir results_v2/cactus_out --logFile PGv2.log --outName Mror_Pangenome_v1 --reference Mror_1466_REFE --giraffe --viz --odgi --chrom-vg --chrom-og --gbz --gfa --vcf --workDir tempdir/
+taskset -c 0-${NSLOTS} singularity exec -H $(pwd) cactus_v2.9.9.sif cactus-pangenome ./tt_v2_2 data/Mror_Genomes.txt --outDir results_v2_2/cactus_out --logFile PGv2.log --outName Mror_Pangenome_v2 --reference Mror_1466_REFE --giraffe --viz --odgi --chrom-vg --chrom-og --gbz --gfa --vcf --workDir tempdir/
